4d34de80d0aec2644a894fdfc2ed6406212c5c30
rowen92 <maksym.pakhn@gmail.com>
2015-12-25 18:56:43 +0200

    edited cart & category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,35 +1,9 @@
 class CategoriesController < ApplicationController
 
-  before_action :set_category, only: [:show, :edit, :update, :destroy]
+  before_action :set_category, only: [:show]
 
   def index
     @categories = Category.all.order(:position)
-  end
-
-  def new
-    @category = Category.new
-  end
-
-  def create
-    @category = Category.new(category_params)
-    if @category.save
-      redirect_to category_path(@category)
-    else
-      render 'new'
-    end
-  end
-
-  def update
-    if @category.update(category_params)
-      redirect_to category_path(@category)
-    else
-      render 'edit'
-    end
-  end
-
-  def destroy
-    @category.destroy
-    redirect_to categories_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
 
   root 'pages#home'
   get 'cart' => 'carts#show'
-
   get 'about' => 'pages#about'
+
   resources :line_items, :categories, :products
 
   # The priority is based upon order of creation: first created -> highest priority.
